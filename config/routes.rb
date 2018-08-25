09dd1cf02a7e26f53fea5dfdf796956ee8a3cba4
taruto <taruto1215@gmail.com>
2018-08-25 12:56:19 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'tasks#index'
