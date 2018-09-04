ff55ce55ad791acf03bd2c6ecda6cc3a8186a60a
taruto <taruto1215@gmail.com>
2018-09-04 14:55:50 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'toppages/index'
+
   get 'users/index'
 
   get 'users/show'
@@ -9,7 +17,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'tasks#index'
+  root to: 'toppages#index'
   
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
